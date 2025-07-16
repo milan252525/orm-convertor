a5e6ef616ac8d7972856315ff05c6e16394cf5af
milan_25 <milan.abraham2@seznam.cz>
2025-07-16 12:32:53 +0200

    Separate demo page

--- ORMConvertor/ORMConvertorAPI/frontend/src/app/app.routes.ts
+++ ORMConvertor/ORMConvertorAPI/frontend/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from "@angular/router";
 import { MainPageComponent } from "./containers/main-page/main-page.component";
+import { DemoPageComponent } from "./containers/demo-page/demo-page.component";
 
 export const routes: Routes = [
   {
     path: "",
     component: MainPageComponent,
+  },
+  {
+    path: "demo",
+    component: DemoPageComponent,
   },
 ];
